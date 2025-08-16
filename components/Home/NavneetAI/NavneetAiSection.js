6c4c6df7b66159bd0d3c02b2d6a0a368696587e0
pooja panwar <poojapanwar@beeonline.co.in>
2025-08-16 18:39:39 +0530

    new-section-added

--- components/Home/NavneetAI/NavneetAiSection.js
+++ components/Home/NavneetAI/NavneetAiSection.js
@@ -2,20 +2,20 @@
 import React from "react";
 import styles from "../../../styles/NavneetAiSection.module.scss";
 
+const paratext = ` Navneet AI, from Navneet Education Limited, is India's first custom
+            AI education model. It offers “Magic Tools” like auto-generated
+            lesson materials (presentations, quizzes, flashcards, summaries), an
+            AI-powered Magic Board, 3D interactive models, educational games,
+            and simulations. Teachers also benefit from a conversational “Magic
+            Bot” co-pilot to enhance teaching.`;
+
 const NavneetAiSection = () => {
   return (
     <div className={styles["navneet-ai-section-content"]}>
       <div className={styles["navneet-ai-left-right-content"]}>
         <div className={styles["navneet-ai-left-content"]}>
           <h2>ELEVATING Learning, ENHANCING TRUST</h2>
-          <p>
-            Navneet AI, from Navneet Education Limited, is India's first custom
-            AI education model. It offers “Magic Tools” like auto-generated
-            lesson materials (presentations, quizzes, flashcards, summaries), an
-            AI-powered Magic Board, 3D interactive models, educational games,
-            and simulations. Teachers also benefit from a conversational “Magic
-            Bot” co-pilot to enhance teaching.
-          </p>
+          <p>{paratext}</p>
           <button>Explore Navneet AI</button>
         </div>
 
