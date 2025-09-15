10158231e101b1bffa59bdf2d4d10078c490b8f8
sujay pagam <sujay@beeonline.co.in>
2025-09-15 11:59:14 +0530

    update: investor- > company-announcements
    submission-under-regulation-30 links and data

--- app/investor/company-announcements/data.js
+++ app/investor/company-announcements/data.js
@@ -5,151 +5,223 @@ export const announcementData = {
     items: [
       {
         text: "Newspaper advertisement of the financial results Q1FY26",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/08/Newspaper-Advertisement_Q126_Signed.pdf",
       },
-      { text: "Audio Recording for earnings call – Q1FY26", link: "#" },
+      {
+        text: "Audio Recording for earnings call – Q1FY26",
+        link: "https://navneet.com/wp-content/uploads/2025/08/WhatsApp-Audio-2025-08-08-at-23.31.55.mp3",
+      },
       {
         text: "Intimation under Regulation 30 – Outcome of Board Meeting held on 07th August, 2025",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/08/Stand-Conso-Results_SE.pdf",
       },
       {
         text: "Intimation under Regulation 30 – Earning Conference Call – Q1FY26",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/08/Earning-Conference-Call_Intimation_Q1_Signed.pdf",
       },
       {
         text: "Intimation of Board Meeting to be held on 07th August, 2025",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/08/Intimation-of-Board-Meeting-to-Be-held-on-07th-August-2025.pdf",
       },
       {
         text: "Disclosure under regulation 30 – Update on Incorporation of WOS",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/05/Regulation-30-Disclosure-Certificate-of-Incorporation_Signed.pdf",
       },
       {
         text: "Disclosure of Transcript of Earning Call for Q4 & FY25",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/05/Transcript-of-Earning-Call_Signed.pdf",
       },
       {
         text: "Newspaper advertisement of the financial results Q4 & FY25",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/05/Covering-Newspaper-Advertisement_Signed.pdf",
       },
-      { text: "Audio recording for Earnings Call – Q4 & FY25", link: "#" },
+      {
+        text: "Audio recording for Earnings Call – Q4 & FY25",
+        link: "https://navneet.com/wp-content/uploads/2025/05/AUD-20250521-WA0000.mp3",
+      },
       {
         text: "Intimation under Regulation 30(6) – Appointment of Secretarial Auditor",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/05/Reg-30-Appointment-of-Secretarial-Auditor_Signed.pdf",
       },
       {
         text: "Intimation under Regulation 30(6) – Outcome of Board Meeting held on 19th May, 2025",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/05/Outcome-of-BM.pdf",
       },
       {
         text: "Intimation under Regulation 30(6) – Disclosure under regulation 30 - Incorporation of WOS",
-        link: "#",
+        link: "https://navneet.com/",
       },
       {
         text: "Intimation under Regulation 30(6) – Earning Conference Call Q4 & FY25",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/05/Earning-Conference-Call_Intimation_Q4_Signed.pdf",
       },
-      { text: "Trading Window Closure – Q4", link: "#" },
+      {
+        text: "Trading Window Closure – Q4",
+        link: "https://navneet.com/wp-content/uploads/2025/03/Trading-window-closure_Signed.pdf",
+      },
       {
         text: "Intimation for Change in the Domain Name and Generic E-mail ID of RTA",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/02/RTA-update_Signed.pdf",
       },
       {
         text: "Disclosure of Transcript of Earning Call for the quarter and nine months ended 31st December, 2024",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/02/Disclosure-Under-Reg-30-Transcript_Signed.pdf",
       },
       {
         text: "Newspaper advertisement of the Un-audited results for the quarter and nine months ended 31st December, 2024",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/02/Newspaper-Advertisement_Reg47.pdf",
       },
-      { text: "Audio recording for Earnings Call – Q3 & 9M FY25", link: "#" },
-      { text: "Integrated Filing (Financials) – Q3 2024", link: "#" },
+      {
+        text: "Audio recording for Earnings Call – Q3 & 9M FY25",
+        link: "https://navneet.com/wp-content/uploads/2025/02/WhatsApp-Audio-2025-02-06-at-17.57.34.mpeg.mp3",
+      },
+      {
+        text: "Integrated Filing (Financials) – Q3 2024",
+        link: "https://navneet.com/wp-content/uploads/2025/02/Integrated-Filing_Signed.pdf",
+      },
       {
         text: "Press Release for the quarter and nine months ended December 2024",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/02/Announcement_Press-Release.pdf",
       },
       {
         text: "Outcome of Board Meeting held on 05th February, 2025",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/02/Outcome-of-BM.pdf",
       },
-      { text: "Disclosure of order passed under Regulation 30", link: "#" },
-      { text: "Disclosure under Regulation 30 – Credit Ratings", link: "#" },
-      { text: "Revised announcement of earning conference call", link: "#" },
+      {
+        text: "Disclosure of order passed under Regulation 30",
+        link: "https://navneet.com/wp-content/uploads/2025/02/Reg-30-disclosure-Order-Passed.pdf",
+      },
+      {
+        text: "Disclosure under Regulation 30 – Credit Ratings",
+        link: "https://navneet.com/wp-content/uploads/2025/02/Disclousure-of-Credit-Ratings_Signed.pdf",
+      },
+      {
+        text: "Revised announcement of earning conference call",
+        link: "https://navneet.com/wp-content/uploads/2025/01/Earning-Conference-Call_Intimation_R_Signed.pdf",
+      },
       {
         text: "Intimation under Regulation 30(6) – Earning Conference Call",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/02/Intimation-under-Regulation-306-Earning-Conference-Call.pdf",
       },
       {
         text: "Intimation of Board Meeting to be held on 5th February, 2025",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2025/01/Revised-Intimation-of-Board-Meeting-to-Be-held-on-05th-January-2025-1.pdf",
       },
-      { text: "Trading Window Closure - Q3", link: "#" },
+      {
+        text: "Trading Window Closure - Q3",
+        link: "https://navneet.com/wp-content/uploads/2025/01/Trading-Window-Closure-Q3_31122024.pdf",
+      },
       {
         text: "Newspaper Advertisement of the Un-audited results for the quarter and half year ended 30th September, 2024",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2024/11/Stock-Exchange-Intimation-Newspaper-Publication.pdf",
       },
-      { text: "Audio Recording for earnings call – Q2 & H1 FY25", link: "#" },
-      { text: "Press Release for half year ended September 2024", link: "#" },
+      {
+        text: "Audio Recording for earnings call – Q2 & H1 FY25",
+        link: "https://navneet.com/wp-content/uploads/2024/11/Audio-Recording-for-earnings-call-%E2%80%93-Q-Q2-H1-FY25.mp3",
+      },
+      {
+        text: "Press Release for half year ended September 2024",
+        link: "https://navneet.com/wp-content/uploads/2024/11/Press-Release-BM-07-11-2024.pdf",
+      },
       {
         text: "Intimation of conference call to discuss Q2 & H1 FY25 Results",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2024/11/Intimation-of-Earning-Call-for-Q2_Signed.pdf",
       },
-      { text: "Intimation for Appointment of CS in WOS", link: "#" },
-      { text: "Proceedings of 38th AGM held on 20th August, 2024", link: "#" },
-      { text: "Audio Recording for earnings call – Q1 FY24-25", link: "#" },
-      { text: "Press Release for quarter ended June 2024", link: "#" },
-      { text: "CSR Impact assessment report", link: "#" },
-      { text: "Trading window closure for Q1’24-25(23-24)", link: "#" },
-      { text: "Audio Recording for earnings call – Q4 (23-24)", link: "#" },
-      { text: "Earning Call Invite – 23rd May, 2024", link: "#" },
+      {
+        text: "Intimation for Appointment of CS in WOS",
+        link: "https://navneet.com/wp-content/uploads/2024/10/Intimation-Under-Regulation-30-Intimation-for-Appointment-of-CS-in-WOS.pdf",
+      },
+      {
+        text: "Proceedings of 38th AGM held on 20th August, 2024",
+        link: "https://navneet.com/wp-content/uploads/2024/08/proceedings.pdf",
+      },
+      {
+        text: "Audio Recording for earnings call – Q1 FY24-25",
+        link: "https://navneet.com/wp-content/uploads/2024/08/Audio-Recording-for-earnings-call-%E2%80%93-Q1-24-25.mp3",
+      },
+      {
+        text: "Press Release for quarter ended June 2024",
+        link: "https://navneet.com/wp-content/uploads/2024/08/Navneet-Education-Limited_Press_Release_Q1FY25.pdf",
+      },
+      {
+        text: "CSR Impact assessment report",
+        link: "https://navneet.com/wp-content/uploads/2024/07/Impact-Assessment-2023-24.pdf",
+      },
+      {
+        text: "Trading window closure for Q1’24-25(23-24)",
+        link: "https://navneet.com/wp-content/uploads/2024/06/letterintimation.pdf",
+      },
+      {
+        text: "Audio Recording for earnings call – Q4 (23-24)",
+        link: "https://navneet.com/wp-content/uploads/2024/05/MNC0220240523153252.mp3",
+      },
+      {
+        text: "Earning Call Invite – 23rd May, 2024",
+        link: "https://navneet.com/wp-content/uploads/2024/05/Boardmeetingintimation-1.pdf",
+      },
       {
         text: "Board Meeting intimation for Approval of Results for the year ended 31st March, 2024",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2024/05/2.intimationletter.pdf",
       },
-      { text: "Intimation under Regulation 30 and press release", link: "#" },
+      {
+        text: "Intimation under Regulation 30 and press release",
+        link: "https://navneet.com/wp-content/uploads/2024/05/IntimationLetterC.pdf",
+      },
       {
         text: "Cancellation of Board Meeting and Approval of Composite Scheme of Arrangement",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2024/05/cancellation.pdf",
       },
       {
         text: "Board Meeting Intimation for Approval Of Financial Results For The Year Ending March 31, 2024 And Recommendation Of A Final Dividend",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2024/05/boardmeetingintimation.pdf",
       },
       {
         text: "Announcement under Regulation 30 (LODR)-Analyst / Investor Meet – Intimation",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2024/05/intimationconferencecall.pdf",
       },
       {
         text: "CRISIL Credit Rating on Commercial Paper and Bank Guarantee",
-        link: "#",
+        link: "https://navneet.com/wp-content/uploads/2024/02/lettercrisil21022024.pdf",
       },
-      { text: "Audio Recording for earnings call – Q3 (23-24)", link: "#" },
-      { text: "Press Release - Q3 (23-24)", link: "#" },
-      { text: "Closure of trading window", link: "#" },
-      { text: "Press Release", link: "#" },
+      {
+        text: "Audio Recording for earnings call – Q3 (23-24)",
+        link: "https://navneet.com/wp-content/uploads/2024/02/earningscallQ3.mp3",
+      },
+      {
+        text: "Press Release - Q3 (23-24)",
+        link: "https://navneet.com/wp-content/uploads/2024/02/Navneet-Education-Limited_Press_Release_Q3FY24-1.pdf",
+      },
+      {
+        text: "Closure of trading window",
+        link: "https://navneet.com/wp-content/uploads/2023/12/tradingwindow.pdf",
+      },
+      {
+        text: "Press Release",
+        link: "https://navneet.com/wp-content/uploads/2023/11/Navneet-Education-Limited_Press_Release_Q2FY24.pdf",
+      },
       {
         text: "Credit rating CRISIL Rating on the Rs.300 Crore Commercial Paper",
-        link: "#",
+        link: "https://navneet.com/pdfs/Investop_Relations_Notice/NAVPUBL_CP_300_07Aug2023_Reval.pdf",
       },
       {
         text: "Intimation Under Regulation 30(9) of SEBI (Listing Obligation And Disclosure Requirements) Regulations 2015",
-        link: "#",
+        link: "https://navneet.com/pdfs/Investop_Relations_Notice/BSE_17.pdf",
       },
       {
         text: "Name and contact details of Key Managerial Personnel under Regulation 30(5) of SEBI",
-        link: "#",
+        link: "https://navneet.com/pdfs/Corporate_Governance_Policies/NEL_CD_15.pdf",
       },
       {
         text: "Intimation under Regulation 30 of SEBI (Listing Obligations and Disclosure Requirements) Regulations, 2015 – COVID-19",
-        link: "#",
+        link: "https://navneet.com/",
       },
       {
         text: "Name and contact details of Key Managerial Personnel under Regulation 30(5) of SEBI",
-        link: "#",
+        link: "https://navneet.com/pdfs/Corporate_Governance_Policies/NEL_CD_15.pdf",
       },
       {
         text: "Intimation Under Regulation 30(9) of SEBI (Listing Obligation And Disclosure Requirements) Regulations 2015",
-        link: "#",
+        link: "https://navneet.com/pdfs/Investop_Relations_Notice/BSE_17.pdf",
       },
     ],
   },
