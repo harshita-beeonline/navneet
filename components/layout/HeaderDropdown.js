a4ffc4829506785c52e287b12cd58dd9f6c4ebf1
sujay pagam <sujay@beeonline.co.in>
2025-09-16 16:04:52 +0530

    Update:  Header Dropdown -> investor links

--- components/layout/HeaderDropdown.js
+++ components/layout/HeaderDropdown.js
@@ -9,44 +9,92 @@ const HeaderDropdown = () => {
     {
       label: "Financials",
       subMenu: [
-        { label: "Quarterly Financial Results", href: "#" },
-        { label: "Investor Presentations", href: "#" },
-        { label: "Annual Reports", href: "#" },
-        { label: "Earnings Call Transcripts", href: "#" },
+        {
+          label: "Quarterly Financial Results",
+          href: "/investor/financials/quarterly-financial-results",
+        },
+        {
+          label: "Investor Presentations",
+          href: "/investor/financials/investor-presentations",
+        },
+        {
+          label: "Annual Reports",
+          href: "/investor/financials/annual-reports",
+        },
+        {
+          label: "Earnings Call Transcripts",
+          href: "/investor/financials/transcripts-of-earnings",
+        },
         { label: "Investors Contact", href: "#" },
       ],
     },
     {
       label: "Company Announcements",
       subMenu: [
-        { label: "Submissions under Regulation 30", href: "#" },
-        { label: "Notice of Board Meeting", href: "#" },
-        { label: "AGM Notices", href: "#" },
-        { label: "Circular in the form of Advertisement inviting Deposits", href: "#" },
-        { label: "Result of e-Voting and Polling at AGM ", href: "#" },
-        { label: "EGM Details", href: "#" },
-        { label: "Unclaimed and Unpaid Amounts", href: "#" },
-        { label: "Buyback 2024", href: "#" },
-        { label: "Others", href: "#" },
+        {
+          label: "Submissions under Regulation 30",
+          href: "/investor/company-announcements/submission-under-regulation-30",
+        },
+        {
+          label: "Notice of Board Meeting",
+          href: "/investor/company-announcements/notice-of-board-meetings",
+        },
+        {
+          label: "AGM Notices",
+          href: "/investor/company-announcements/agm-notices",
+        },
+        {
+          label: "Circular in the form of Advertisement inviting Deposits",
+          href: "/investor/company-announcements/circular-inviting-deposits",
+        },
+        {
+          label: "Result of e-Voting and Polling at AGM ",
+          href: "/investor/company-announcements/result-of-evoting-and-poll",
+        },
+        { label: "EGM Details", href: "/" },
+        {
+          label: "Unclaimed and Unpaid Amounts",
+          href: "/investor/company-announcements/unclaimed-and-unpaid-amounts",
+        },
+        {
+          label: "Buyback 2024",
+          href: "/investor/company-announcements/buyback-2024",
+        },
+        { label: "Others", href: "/investor/company-announcements/others" },
       ],
     },
     {
       label: "Investor Information",
       subMenu: [
-        { label: "Quarterly Shareholding Pattern", href: "#" },
-        { label: "Historical Financial Snapshot", href: "#" },
-        { label: "Stock Information FY 22-23", href: "#" },
+        {
+          label: "Quarterly Shareholding Pattern",
+          href: "/investor/investor-information/quarterly-shareholding-pattern",
+        },
+        {
+          label: "Historical Financial Snapshot",
+          href: "/investor/investor-information/historical-financial-snapshot",
+        },
+        {
+          label: "Stock Information FY 22-23",
+          href: "/investor/investor-information/stock-information",
+        },
       ],
     },
     {
       label: "Corporate Governance",
       subMenu: [
-        { label: "Board of Directors and its commitees", href: "#" },
-        { label: "Code of Conduct", href: "#" },
-        { label: "Policies", href: "#" },
+        {
+          label: "Board of Directors and its commitees",
+          href: "/investor/corporate-governance/board-of-directors-committee",
+        },
+        {
+          label: "Code of Conduct",
+          href: "/investor/corporate-governance/code-of-conduct",
+        },
+        { label: "Policies", href: "/investor/corporate-governance/policies" },
       ],
     },
-    { label: "Investor Contact", href: "#" },
+    { label: "Investor Contact", href: "/investor/contact" },
   ];
 
   return (
