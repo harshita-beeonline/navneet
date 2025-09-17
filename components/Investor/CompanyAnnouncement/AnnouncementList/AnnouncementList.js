b873dace76ffc9e5f347034d6348dda73c144184
sujay pagam <sujay@beeonline.co.in>
2025-09-17 11:27:57 +0530

    Fixing build : import issue

--- components/Investor/CompanyAnnouncement/AnnouncementList/AnnouncementList.js
+++ components/Investor/CompanyAnnouncement/AnnouncementList/AnnouncementList.js
@@ -1,4 +1,4 @@
-import styles from "../../../styles/Investor/CompanyAnnouncements/AnnouncementList.module.scss";
+import styles from "../../../../styles/Investor/CompanyAnnouncements/AnnouncementList.module.scss";
 
 export default function AnnouncementList({ items }) {
   return (
