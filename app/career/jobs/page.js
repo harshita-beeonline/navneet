b7c573ef8baeb770973d051c3ce645c440cc97c7
sujay pagam <sujay@beeonline.co.in>
2025-09-22 17:14:31 +0530

    career listing page

--- app/career/jobs/page.js
+++ app/career/jobs/page.js
@@ -3,61 +3,75 @@ import styles from "../../../styles/Career/JobsPage.module.scss";
 import JobCard from "@/components/Career/JobCard/JobCard";
 
 import { getAllJobs, getJobsByCategory } from "../../../lib/jobs";
+import Link from "next/link";
 
 export default function JobsPage({ searchParams }) {
   const category = searchParams?.category || null;
   const jobs = category ? getJobsByCategory(category) : getAllJobs();
 
   return (
-    <main className={styles["job-page"]}>
-      {/* Heading */}
-      <div className={styles["job-page-heading"]}>Current Openings</div>
+    <>
+      <main className={styles["job-page"]}>
+        {/* Heading */}
+        <div className={styles["job-page-heading"]}>Current Openings</div>
 
-      {/* Search Section */}
-      <div className={styles["search-bar-section"]}>
-        <div className={styles["open-jobs"]}>40 Open Jobs</div>
+        {/* Search Section */}
+        <div className={styles["search-bar-section"]}>
+          <div className={styles["open-jobs"]}>40 Open Jobs</div>
 
-        <div className={styles["search-bar"]}>
-          <CiSearch className={styles["search-icon"]} />
-          <input
-            type="text"
-            placeholder="Enter keywords to search"
-            className={styles["search-input"]}
-          />
-          <button className={styles["search-btn"]}>Search</button>
-        </div>
-      </div>
-
-      {/* Filters */}
-      <div className={styles["filters-section"]}>
-        <div className={styles["filter-box"]}>
-          <label>Location</label>
-          <div className={styles["select-wrapper"]}>
-            <select>
-              <option>All Location</option>
-            </select>
+          <div className={styles["search-bar"]}>
+            <CiSearch className={styles["search-icon"]} />
+            <input
+              type="text"
+              placeholder="Enter keywords to search"
+              className={styles["search-input"]}
+            />
+            <button className={styles["search-btn"]}>Search</button>
           </div>
         </div>
-        <div className={styles["filter-box"]}>
-          <label>Employment Type</label>
-          <div className={styles["select-wrapper"]}>
-            <select>
-              <option>All</option>
-            </select>
+
+        {/* Filters */}
+        <div className={styles["filters-section"]}>
+          <div className={styles["filter-box"]}>
+            <label>Location</label>
+            <div className={styles["select-wrapper"]}>
+              <select>
+                <option>All Location</option>
+              </select>
+            </div>
+          </div>
+          <div className={styles["filter-box"]}>
+            <label>Employment Type</label>
+            <div className={styles["select-wrapper"]}>
+              <select>
+                <option>All</option>
+              </select>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* job listing */}
-      <div className={styles["job-header"]}>
-        <div>Role</div>
-        <div>Location</div>
-        <div>Job Type</div>
-      </div>
+        {/* Job Listing */}
+        <div className={styles["job-header"]}>
+          <div>Role</div>
+          <div>Location</div>
+          <div>Job Type</div>
+        </div>
 
-      {jobs.map((job) => (
-        <JobCard job={job} key={job.id} />
-      ))}
-    </main>
+        {jobs.map((job) => (
+          <JobCard job={job} key={job.id} />
+        ))}
+      </main>
+
+      {/* Join Talent pool */}
+      <div className={styles["join-talent-pool"]}>
+        <div className={styles["heading"]}>Join Talent Pool</div>
+        <div className={styles["info"]}>
+          Join our talent pool by simply submitting your resume. We’ll inform
+          you about the new jobs matching your profile and update you if you are
+          the best fit for one of our open positions.
+        </div>
+        <Link href={"/"} className={styles["btn"]}>Submit resume</Link>
+      </div>
+    </>
   );
 }
