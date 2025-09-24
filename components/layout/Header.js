13e993c6002fe885375ae1f38b943858bb510122
sujay pagam <sujay@beeonline.co.in>
2025-09-24 12:24:21 +0530

    updated header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { RiMenu3Line } from "react-icons/ri";
 
 import styles from "../../styles/Header.module.scss";
 import logo from "../../public/images/LogoNavneet.svg";
@@ -54,7 +55,7 @@ export default function HomePage() {
       </Link>
 
       <div className={styles["menu-toggle"]} id="menu-toggle">
-        <svg width="24" height="10" viewBox="0 0 24 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+        {/* <svg width="24" height="10" viewBox="0 0 24 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M23.5293 1.59953C23.5293 1.09111 23.1171 0.678955 22.6087 0.678955H1.52369C1.01527 0.678955 0.603113 1.09111 0.603113 1.59953C0.603113 2.10795 1.01527 2.52011 1.52369 2.52011H22.6087C23.1171 2.52011 23.5293 2.10795 23.5293 1.59953Z"
             fill="#000"
@@ -63,7 +64,8 @@ export default function HomePage() {
             d="M23.5303 8.96403C23.5303 8.45561 23.1181 8.04346 22.6097 8.04346H7.77726C7.26884 8.04346 6.85669 8.45561 6.85669 8.96403C6.85669 9.47246 7.26884 9.88461 7.77726 9.88461H22.6097C23.1181 9.88461 23.5303 9.47246 23.5303 8.96403Z"
             fill="#000"
           />
-        </svg>
+        </svg> */}
+        <RiMenu3Line/>
       </div>
 
       <nav className={`${styles["nav-menu"]} ${isMenuOpen ? styles["active"] : ""}`} id="nav-menu">
