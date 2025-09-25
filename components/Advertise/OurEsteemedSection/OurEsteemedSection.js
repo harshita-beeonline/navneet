b8ffcce689d0cf5382caf3918a5e865f982d3da7
Harshita <harshita@beeonline.co.in>
2025-09-25 11:51:44 +0530

    added comment

--- components/Advertise/OurEsteemedSection/OurEsteemedSection.js
+++ components/Advertise/OurEsteemedSection/OurEsteemedSection.js
@@ -8,6 +8,7 @@ import img2 from "../../../public/images/advertise/imgbox2.png";
 import stationary from "../../../public/images/advertise/stationary-box.png";
 import production from "../../../public/images/advertise/production-box.png";
 
+//new changes
 const OurEsteemedSection = () => {
   return (
     <section className={styles.pageWrapper}>
