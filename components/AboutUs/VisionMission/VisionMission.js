dccb56cd05af7cfa6f58f435cf28930e7a8bb40d
sujay pagam <sujay@beeonline.co.in>
2025-10-30 13:33:31 +0530

    update

--- components/AboutUs/VisionMission/VisionMission.js
+++ components/AboutUs/VisionMission/VisionMission.js
@@ -1,60 +1,75 @@
 import React from "react";
+import Image from "next/image";
 import styles from "../../../styles/AboutUs/VisionMission.module.scss";
 
-// Import SVGs directly (works better for background)
-import VisionBg from "../../../public/images/about/Vision.svg";
-import MissionBg from "../../../public/images/about/Mission.svg";
+import vision_mission_bg from "../../../public/images/about/vision_mission_bg.jpg";
+import VisionIcon from "../../../public/images/about/vision.png";
+import MissionIcon from "../../../public/images/about/mission.png";
 
-const VisionMission = () => {
+export default function VisionMission() {
   return (
     <section className={styles["visionMissionSection"]}>
-      <div className={styles["container"]}>
+      <div className={styles["bgWrap"]} aria-hidden>
+        <Image
+          src={vision_mission_bg}
+          alt="bookshelf background"
+          fill
+          sizes="100vw"
+          style={{ objectFit: "cover" }}
+        />
+        <div className={styles["overlay"]} />
+      </div>
 
-        {/* Vision Card */}
-        <div className={styles["card"]}>
-          <div className={styles["headingWrapper"]}>
-            <h2 className={styles["subTitle"]}>Vision</h2>
-            <img
-              src={VisionBg.src}
-              alt="Vision Background"
-              className={styles["bgIcon"]}
+      <div className={styles["container"]}>
+        {/* Vision column */}
+        <div className={styles["column"]}>
+          <div className={styles["iconWrap"]}>
+            <Image
+              src={VisionIcon}
+              alt="vision icon"
+              width={300}
+              height={200}
+              priority
             />
           </div>
-          <ul className={styles["list"]}>
-            <li>
-              To provide the highest quality of educational products and
-              services to customers in the language/medium of their choice.
-            </li>
-          </ul>
+
+          <h2 className={styles["title"]}>Vision</h2>
+
+          <p className={styles["description"]}>
+            To provide the highest quality of educational products and services
+            to customers in the language/medium of their choice.
+          </p>
         </div>
 
-        {/* Mission Card */}
-        <div className={styles["card"]}>
-          <div className={styles["headingWrapper"]}>
-            <h2 className={styles["subTitle"]}>Mission</h2>
-            <img
-              src={MissionBg.src}
-              alt="Mission Background"
-              className={styles["bgIcon"]}
+        <div className={styles["divider"]} />
+
+        {/* Mission column */}
+        <div className={styles["column"]}>
+          <div className={styles["iconWrap"]}>
+            <Image
+              src={MissionIcon}
+              alt="mission icon"
+              width={300}
+              height={217}
+              priority
             />
           </div>
-          <ul className={styles["list"]}>
+
+          <h2 className={styles["title"]}>Mission</h2>
+
+          <ul className={styles["points"]}>
             <li>
               To provide students with best quality supplementary study material
               and curriculum text books at affordable price.
             </li>
             <li>
               To harness the power of Information Technology and bring home its
-              wonder to children through a learning.
+              wonder to children through a learning
             </li>
-            <li>
-              To provide students with scholastic stationery products.
-            </li>
+            <li>To provide students with scholastic stationery products.</li>
           </ul>
         </div>
       </div>
     </section>
   );
-};
-
-export default VisionMission;
+}
