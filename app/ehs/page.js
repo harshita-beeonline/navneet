1e1d65544d4775e84ed82c6a60f37b4f2f9ac24c
sujay pagam <sujay@beeonline.co.in>
2025-11-01 11:55:38 +0530

    Update page.js

--- app/ehs/page.js
+++ app/ehs/page.js
@@ -6,18 +6,22 @@ import PoliciesSection from "../../components/EHS/PoliciesSection/PoliciesSectio
 import PillarsSection from "../../components/EHS/PillarsSection/PillarsSection";
 import Pillars from "../../components/EHS/Pillars/Pillars";
 import EhsSection from "../../components/EHS/EhsSection/EhsSection";
+
 export default function HomePage() {
   return (
-    <main>
+    <main
+      style={{
+        background:
+          "linear-gradient(174.05deg, #FFF7ED -1.72%, #E8F7FF 61.4%, #D9E9F8 106.39%)",
+      }}
+    >
       {/* Hero Section */}
       <HeroSection />
-      <VMCSection/>
-      <PoliciesSection/>
-      <PillarsSection/>
-      <Pillars/>
-      <EhsSection/>
-     
-
+      <VMCSection />
+      <PoliciesSection />
+      <PillarsSection />
+      <Pillars />
+      <EhsSection />
     </main>
   );
 }
